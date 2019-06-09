1341739ce165ec6df2d9138eb38b472ad24f366c
antoine faravelon <antoine.faravelon@greenwaves-technologies.com>
2019-06-09 17:09:37 +0200

    Revert "Changed type slot postition in cluster conf struct, which leads problems"
    This reverts commit 1e91846de3b6e805962b990d0de5b1ee8a5b91a8

--- include/pmsis_cluster/cl_pmsis_types.h
+++ include/pmsis_cluster/cl_pmsis_types.h
@@ -78,8 +78,8 @@ typedef struct cluster_driver_api {
 
 typedef struct cluster_driver_conf {
     // do not move this one, might be accessed in various hackish way
-    int id;
     pi_device_e device_type;
+    int id;
     void *heap_start;
     uint32_t heap_size;
     struct pmsis_event_kernel_wrap* event_kernel;
