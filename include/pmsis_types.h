9ccf11de62b69f7b04bd648ab85f5e9f3293b1f6
Antoine Faravelon <antoine.faravelon@greenwaves-technologies.com>
2019-07-17 13:34:18 +0200

    Small fixes:     + remove INLINE macro where it is not defined and does not make     sense     + clean up some specific types

--- include/pmsis_types.h
+++ include/pmsis_types.h
@@ -114,14 +114,15 @@ typedef struct pi_device_api {
 } pi_device_api_t;
 
 #ifndef IMPLEM_MUTEX_OBJECT_TYPE
-#define IMPLEM_MUTEX_OBJECT_TYPE void*
+#define IMPLEM_MUTEX_OBJECT_TYPE \ 
+    void* mutex_object;
 #endif
 
 /** Task types **/
-typedef void (*__pmsis_mutex_func)(IMPLEM_MUTEX_OBJECT_TYPE mutex_object);
+typedef void (*__pmsis_mutex_func)(void *mutex_object);
 
 typedef struct pmsis_mutex {
-    IMPLEM_MUTEX_OBJECT_TYPE mutex_object;
+    IMPLEM_MUTEX_OBJECT_TYPE
     __pmsis_mutex_func take;
     __pmsis_mutex_func release;
 } pmsis_mutex_t;
