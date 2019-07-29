1be8e8a619a01896b92d4721bf1d244df900b8d1
Antoine Faravelon <antoine.faravelon@greenwaves-technologies.com>
2019-07-29 11:25:33 +0200

    fix interaction with l2 malloc for alios

--- include/rtos/malloc/pmsis_l2_malloc.h
+++ include/rtos/malloc/pmsis_l2_malloc.h
@@ -1,10 +1,10 @@
 #ifndef __PMSIS_L2_MALLOC_H__
 #define __PMSIS_L2_MALLOC_H__
 
-#include "pmsis_types.h"
-#include "pmsis_malloc_internal.h"
+#include "pmsis.h"
+#include "rtos/malloc/pmsis_malloc_internal.h"
 
-
+#ifndef __L2_MALLOC_NATIVE__
 void *pmsis_l2_malloc(int size);
 
 void pmsis_l2_malloc_free(void *_chunk, int size);
@@ -16,5 +16,5 @@ void pmsis_l2_malloc_init(void *heapstart, uint32_t size);
 void pmsis_l2_malloc_set_malloc_struct(malloc_t malloc_struct);
 
 malloc_t pmsis_l2_malloc_get_malloc_struct(void);
-
+#endif
 #endif
