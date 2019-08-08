e6f88352b37dc2b6a826d8dd00b594b5af6d273c
S. Sivarajah <sivarajah.sivapiriyan@greenwaves-technologies.com>
2019-08-08 11:49:41 +0200

    Update cleanup, remove pmsis_driver_core(use device.h), mv pmsis_task in os_front_end

--- include/pmsis/task.h
+++ include/pmsis/task.h
@@ -7,7 +7,7 @@ pi_task_t *pi_task_callback(pi_task_t *callback_task, void (*callback)(void*), v
 
 pi_task_t *pi_task_callback_no_mutex(pi_task_t *callback_task, void (*func)(void *), void *arg);
 
-static inline pi_task_t *pi_task_block(pi_task_t *callback_task);
+pi_task_t *pi_task_block(pi_task_t *callback_task);
 
 pi_task_t *pi_task_block_no_mutex(pi_task_t *callback_task);
 
@@ -29,7 +29,7 @@ void pi_task_wait_on_no_mutex(pi_task_t *task);
 
 #ifdef PMSIS_DRIVERS
 
-#include "pmsis_hal/pmsis_hal.h"
+//#include "pmsis_hal/pmsis_hal.h"
 #include "pmsis_backend/pmsis_backend_native_task_api.h"
 
 static inline struct pi_task *pi_task(struct pi_task *task)
@@ -37,11 +37,8 @@ static inline struct pi_task *pi_task(struct pi_task *task)
     pi_task_block(task);
     return task;
 }
-
 #else
-
 #include "pmsis/implem/implem.h"
+#endif
 
 #endif  /* __PMSIS_TASK_H__ */
-
-#endif
