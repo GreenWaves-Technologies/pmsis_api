41b09da2fe1fc844a5d9f41f007497937dd5fc7d
Germain Haugou <germain.haugou@iis.ee.ethz.ch>
2019-10-14 11:22:45 +0200

    Added allocator doc

--- include/pmsis/rtos/malloc/pmsis_fc_tcdm_malloc.h
+++ include/pmsis/rtos/malloc/pmsis_fc_tcdm_malloc.h
@@ -20,16 +20,74 @@
 #include "pmsis/rtos/malloc/pmsis_malloc_internal.h"
 
 #if (__FC_MALLOC_NATIVE__ == 0)
-void *pmsis_fc_tcdm_malloc(uint32_t size);
 
-void pmsis_fc_tcdm_malloc_free(void *_chunk, int size);
+/**
+* @ingroup groupRTOS
+*/
 
-void *pmsis_fc_tcdm_malloc_align(int size, int align);
+/**
+ * @defgroup MemAlloc Memory allocation
+ *
+ * This provides support for memory allocation.
+ */
+
+/**        
+ * @addtogroup MemAlloc
+ * @{        
+ */
+
+/**@{*/
+
+/** \brief Allocate FC TCDM memory.
+ *
+ * The allocated memory is 4-bytes aligned.
+ * The caller has to provide back the size of the allocated chunk when freeing
+ * it.
+ *
+ * \param size   The size in bytes of the memory to be allocated.
+ * \return The allocated chunk or NULL if there was not enough memory available.
+ */
+void *pi_fc_tcdm_malloc(uint32_t size);
+
+/** \brief Free FC TCDM memory.
+ *
+ * \param chunk  The chunk to be freed.
+ * \param size   The size in bytes of the memory to be freed.
+ */
+void pi_fc_tcdm_free(void *chunk, int size);
+
+/** \brief Allocate FC TCDM memory.
+ *
+ * The allocated memory is aligned on the specified number of bytes.
+ * The caller has to provide back the size of the allocated chunk when freeing
+ * it.
+ *
+ * \param size   The size in bytes of the memory to be allocated.
+ * \param align  The alignment in bytes.
+ * \return The allocated chunk or NULL if there was not enough memory available.
+ */
+void *pi_fc_tcdm_malloc_align(int size, int align);
+
+//!@}
+
+/**        
+ * @} 
+ */
+
+/// @cond IMPLEM
 
 void pmsis_fc_tcdm_malloc_init(void *heapstart, uint32_t size);
 
 void pmsis_fc_tcdm_malloc_set_malloc_struct(malloc_t malloc_struct);
 
 malloc_t pmsis_fc_tcdm_malloc_get_malloc_struct(void);
+
+#define pmsis_fc_tcdm_malloc pi_fc_tcdm_malloc
+#define pmsis_fc_tcdm_malloc_align pi_fc_tcdm_malloc_align
+#define pmsis_fc_tcdm_malloc_free pi_fc_tcdm_free
+
+/// @endcond
+
 #endif
+
 #endif  /* __PMSIS_FC_TCDM_MALLOC_H__ */
--- include/pmsis/rtos/malloc/pmsis_l2_malloc.h
+++ include/pmsis/rtos/malloc/pmsis_l2_malloc.h
@@ -17,24 +17,79 @@
 #ifndef __PMSIS_L2_MALLOC_H__
 #define __PMSIS_L2_MALLOC_H__
 
+/**
+* @ingroup groupRTOS
+*/
+
+/**
+ * @defgroup MemAlloc Memory allocation
+ *
+ * This provides support for memory allocation.
+ */
+
+/**        
+ * @addtogroup MemAlloc
+ * @{        
+ */
+
+/**@{*/
+
 #include "pmsis/rtos/malloc/pmsis_malloc_internal.h"
 
 #if !defined(__L2_MALLOC_NATIVE__) || (__L2_MALLOC_NATIVE__ == 0)
 
-void *pmsis_l2_malloc(int size);
+/** \brief Allocate L2 memory.
+ *
+ * The allocated memory is 4-bytes aligned.
+ * The caller has to provide back the size of the allocated chunk when freeing
+ * it.
+ *
+ * \param size   The size in bytes of the memory to be allocated.
+ * \return The allocated chunk or NULL if there was not enough memory available.
+ */
+void *pi_l2_malloc(int size);
 
-void pmsis_l2_malloc_free(void *_chunk, int size);
+/** \brief Free L2 memory.
+ *
+ * \param chunk  The chunk to be freed.
+ * \param size   The size in bytes of the memory to be freed.
+ */
+void pi_l2_free(void *chunk, int size);
 
-void *pmsis_l2_malloc_align(int size, int align);
+/** \brief Allocate L2 memory.
+ *
+ * The allocated memory is aligned on the specified number of bytes.
+ * The caller has to provide back the size of the allocated chunk when freeing
+ * it.
+ *
+ * \param size   The size in bytes of the memory to be allocated.
+ * \param align  The alignment in bytes.
+ * \return The allocated chunk or NULL if there was not enough memory available.
+ */
+void *pi_l2_malloc_align(int size, int align);
 
-void pmsis_l2_malloc_init(void *heapstart, uint32_t size);
+//!@}
 
-void pmsis_l2_malloc_dump();
+/**        
+ * @} 
+ */
+
+#endif  /* __L2_MALLOC_NATIVE__ */
+
+/// @cond IMPLEM
 
 //void pmsis_l2_malloc_set_malloc_struct(malloc_t malloc_struct);
 
 //malloc_t pmsis_l2_malloc_get_malloc_struct(void);
 
-#endif  /* __L2_MALLOC_NATIVE__ */
+void pmsis_l2_malloc_init(void *heapstart, uint32_t size);
+
+void pmsis_l2_malloc_dump();
+
+#define pmsis_l2_malloc pi_l2_malloc
+#define pmsis_l2_malloc_align pi_l2_malloc_align
+#define pmsis_l2_malloc_free pi_l2_free
+
+/// @endcond
 
 #endif  /* __PMSIS_L2_MALLOC_H__ */
